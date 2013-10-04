4ea7644346e3310b9b5bd11671c53724fba7439c
Ken Gullaksen <ken.gullaksen@embriq.no>
2013-10-04 14:37:20 +0200

    added plugin, and readme

---
+++ PyramidSort.py
@@ -0,0 +1,17 @@
+import sublime, sublime_plugin
+
+def pyramid_sort(txt):
+	txt = filter(None, txt)
+	txt.sort(key = lambda s: len(s))
+	return txt
+
+class PyramidSortCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		regions = [s for s in self.view.sel() if not s.empty()]
+		if regions:			
+			for r in regions:
+				lr = self.view.line(r)
+				txt = self.view.substr(lr)
+				lines = txt.splitlines()
+				lines = pyramid_sort(lines)
+				self.view.replace(edit, lr, u"\n".join(lines))
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-sublime-text-pyramid-sort
-=========================
+PyramidSortSublimeTextPlugin
+============================
 
-Pyramid Sort plugin for Sublime text
+Sorts selected text based on line length, from shortest to longest, while cutting out empty lines
+
+So this:
+    foobar
+    bazolino
+
+    barolainen
+    foo
+
+Becomes:
+    foo
+	foobar
+    bazolino
+    barolainen
+
+# Installing
+
+
